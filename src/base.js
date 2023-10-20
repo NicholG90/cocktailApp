65f1e43368c15c56b0fe3e95390a63f56d9c1c43
ngourlay <ngourlay@rentsync.com>
2023-10-20 18:31:58 -0400

    update to react 18 - don't not commit package.json dumbass

--- src/base.js
+++ src/base.js
@@ -1,56 +1,56 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-import "firebase/compat/database"
+import "firebase/compat/database";
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 export default app;
 
 export const auth = firebase.auth();
-const googleProvider = new firebase.auth.GoogleAuthProvider()
+const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => {
   auth.signInWithPopup(googleProvider).then((res) => {
   }).catch((error) => {
-    console.log(error.message)
-  })
-}
+    console.log(error.message);
+  });
+};
 
-const twitterProvider = new firebase.auth.TwitterAuthProvider()
+const twitterProvider = new firebase.auth.TwitterAuthProvider();
 
 export const signInWithTwitter = () => {
   auth.signInWithPopup(twitterProvider).then((res) => {
   }).catch((error) => {
-    console.log(error.message)
-  })
-}
+    console.log(error.message);
+  });
+};
 
-const facebookProvider = new firebase.auth.FacebookAuthProvider()
+const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
 export const signInWithFacebook = () => {
   auth.signInWithPopup(facebookProvider).then((res) => {
   }).catch((error) => {
-    console.log(error.message)
-  })
-}
+    console.log(error.message);
+  });
+};
 
 
 
 export const logOut = () => {
-    auth.signOut().then(()=> {
-      console.log('logged out')
-    }).catch((error) => {
-      console.log(error.message)
-    })
-  }
+  auth.signOut().then(() => {
+    console.log('logged out');
+  }).catch((error) => {
+    console.log(error.message);
+  });
+};
 
 export const db = firebase.database().ref();
 
--- src/cocktailresult.js
+++ src/cocktailresult.js
@@ -1,45 +1,46 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import CocktailDisplay from "./cocktaildisplay";
-import CocktailAnimation from './cocktailanimation'
+import CocktailAnimation from './cocktailanimation';
 
 function CocktailResult(props) {
     const [data, setData] = useState([]);
-    const [cocktailId, setCocktailId] = useState ([]);
-    const [isLoading, setIsLoading] = useState(true)
+    const [cocktailId, setCocktailId] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const query = props.data;
 
 
     const parts = [];
-    for ( var i = 0; i < query.length; ++i )
-    parts.push(encodeURIComponent(query[i].value));
+    for (var i = 0; i < query.length; ++i)
+        parts.push(encodeURIComponent(query[i].value));
     const url = parts.join(',');
 
     useEffect(() => {
         if (query !== undefined) {
             fetchIngredients();
-            } else {
-            console.log('no selection')
-            }
+        } else {
+            console.log('no selection');
+        }
         async function fetchIngredients() {
             try {
-                const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_COCKTAILDB}/filter.php?i=${url}`)
+                const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/${process.env.REACT_APP_COCKTAILDB}/filter.php?i=${url}`);
                 const apiData = await response.json();
-                const drinks = apiData.drinks
+                const drinks = apiData.drinks;
                 const randint = Math.floor(Math.random() * drinks.length);
-                setCocktailId(apiData.drinks[randint].idDrink)
+                setCocktailId(apiData.drinks[randint].idDrink);
                 setIsLoading(true);
             } catch (err) {
-            console.log(err);        }
+                console.log(err);
+            }
         }
-        }, [query, url] );
+    }, [query, url]);
 
 
     useEffect(() => {
-        if (cocktailId) {
+        if (cocktailId && cocktailId.length > 0) {
             listingInformation();
-            } else {
+        } else {
             setIsLoading(false);
-            }
+        }
         async function listingInformation() {
             try {
                 const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${cocktailId}`);
@@ -47,25 +48,25 @@ function CocktailResult(props) {
                 setIsLoading(false);
                 setData(apiData.drinks[0]);
             } catch (err) {
-            console.log(err);
+                console.log(err);
+            }
         }
-        }
-        }, [cocktailId] );
+    }, [cocktailId]);
 
 
-        if (isLoading === true)
+    if (isLoading === true)
         return (
-        <div>
-            <header>
-                <CocktailAnimation props={'rocksGlassLoading'}/>
-            </header>
-        </div>
-        )
-        return (
-        <div>
-                {(cocktailId === undefined) ? <h2>Couldn't find anything with those ingrdients</h2> :  <CocktailDisplay data={data} />}
-        </div>
+            <div>
+                <header>
+                    <CocktailAnimation props={'rocksGlassLoading'} />
+                </header>
+            </div>
         );
-    }
-    
-    export default CocktailResult;
+    return (
+        <div>
+            {(cocktailId === undefined) ? <h2>Couldn't find anything with those ingrdients</h2> : <CocktailDisplay data={data} />}
+        </div>
+    );
+}
+
+export default CocktailResult;
